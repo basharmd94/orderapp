80240170b0f32a5b7e28acf79d1318ed55b1208e
basharmd94 <mat197150@gmail.com>
2024-04-30 10:55:42 +0600

    day-3

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
-
+from routers import opord_route
 
 app = FastAPI()
 
-
-@app.get("/getindex")
-async def getindex():
-    return {"data" : "this is a index page"}
-
-
-
-
+app.include_router(opord_route.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9800)
