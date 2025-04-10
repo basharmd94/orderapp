11efaf61cda3a514646d8125ac5a7853b17fc234
basharmd94 <mat197150@gmail.com>
2025-04-10 16:01:27 +0600

    Final Code submitted

--- mobileapp/metro.config.js
+++ mobileapp/metro.config.js
@@ -1,9 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
-
+  
 const config = getDefaultConfig(__dirname);
-config.resolver = {
-  ...config.resolver,
-  sourceExts: ['js', 'jsx', 'ts', 'tsx'], // Explicitly support JS and TS
-};
+  
 module.exports = withNativeWind(config, { input: './global.css' });
